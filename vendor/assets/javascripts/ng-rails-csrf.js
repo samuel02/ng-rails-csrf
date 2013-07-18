d189c3f581c123fbb409f70c9ad4a926b01904c6
Samuel Nilsson <samuel02@gmail.com>
2013-07-18 12:32:37 +0200

    Use angular element for compatibility with IE7

--- vendor/assets/javascripts/ng-rails-csrf.js
+++ vendor/assets/javascripts/ng-rails-csrf.js
@@ -1,12 +1,12 @@
 angular.module('ng-rails-csrf', [] ).config(['$httpProvider', function($httpProvider) {    
     var getToken = function() { 
         // Rails 3+
-        var el = document.querySelector('meta[name="csrf-token"]');
+        var el = angular.element('meta[name="csrf-token"]');
         if (el) {
-            el = el.getAttribute('content');
+            el = el.attr('content');
         } else {
             // Rails 2
-            el = document.querySelector('input[name="authenticity_token"]');
+            el = angular.element('input[name="authenticity_token"]');
             if (el) {
                 el = el.value;
             }
@@ -22,6 +22,6 @@ angular.module('ng-rails-csrf', [] ).config(['$httpProvider', function($httpProv
     };    
     updateToken();    
     if (window['Turbolinks']) {
-      $(document).bind('page:change', updateToken); 
+      angular.element(document).bind('page:change', updateToken); 
     }
 }]);
